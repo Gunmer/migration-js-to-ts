5aee1ed05e9c303f5a22ce1ac596d1e201b94536
Cristiam Sosa <csosaur@gmail.com>
2019-05-06 20:51:24 +0200

    Test parallel request

--- src/playground.ts
+++ src/playground.ts
@@ -1,61 +1,62 @@
-import {Task} from "./playground/task";
-import Axios, {AxiosInstance, AxiosPromise} from "axios";
+import Axios from "axios";
 
 class Playground {
 
-    run(){
+    http = Axios.create({});
+
+    run() {
+        this.http.interceptors.request.use(this.logReq());
+        this.http.interceptors.response.use(this.logRes());
         this.executeSerial();
         this.executeParallel();
+    }
+
+    logReq() {
+        return request => {
+            request.metadata = {startTime: new Date()};
+            //console.log(`startTime: ${request.metadata.startTime}`);
+            return request;
+        }
+    }
+
+    logRes() {
+        return response => {
+            response.config.metadata.endTime = new Date();
+            response.duration = response.config.metadata.endTime - response.config.metadata.startTime;
+            //console.log(`endTime: ${response.config.metadata.endTime}`);
+            console.log(`duration: ${response.duration}`);
+            return response;
+        };
     }
 
     private async executeParallel() {
         let t0 = Date.now();
 
-        let http = Axios.create({});
-        let promise1 = http.get(`https://jsonplaceholder.typicode.com/posts/1`);
-        let promise2 = http.get(`https://jsonplaceholder.typicode.com/posts/2`);
-        let promise3 = http.get(`https://jsonplaceholder.typicode.com/posts/3`);
-        let promise4 = http.get(`https://jsonplaceholder.typicode.com/posts/4`);
+        let promise1 = this.http.get(`https://jsonplaceholder.typicode.com/posts`);
+        let promise2 = this.http.get(`https://jsonplaceholder.typicode.com/posts`);
+        let promise3 = this.http.get(`https://jsonplaceholder.typicode.com/posts`);
+        let promise4 = this.http.get(`https://jsonplaceholder.typicode.com/posts`);
 
         let [response1] = await Promise.all([promise1, promise2, promise3, promise4]);
 
         let t1 = Date.now();
         console.log(`Execute in parallel: ${t1 - t0} milliseconds`);
-        console.log(`Results: ${JSON.stringify([response1.data])}`);
+        //console.log(`Results: ${JSON.stringify([response1.data])}`);
     }
 
     private async executeSerial() {
         let t0 = Date.now();
 
-        let http = Axios.create({});
-        let response1 = await http.get(`https://jsonplaceholder.typicode.com/posts/1`);
-        let response2 = await http.get(`https://jsonplaceholder.typicode.com/posts/2`);
-        let response3 = await http.get(`https://jsonplaceholder.typicode.com/posts/3`);
-        let response4 = await http.get(`https://jsonplaceholder.typicode.com/posts/4`);
+        let response1 = await this.http.get(`https://jsonplaceholder.typicode.com/posts`);
+        let response2 = await this.http.get(`https://jsonplaceholder.typicode.com/posts`);
+        let response3 = await this.http.get(`https://jsonplaceholder.typicode.com/posts`);
+        let response4 = await this.http.get(`https://jsonplaceholder.typicode.com/posts`);
 
         let t1 = Date.now();
         console.log(`Execute in serial: ${t1 - t0} milliseconds`);
-        console.log(`Results: ${JSON.stringify(response1.data)}`);
+        //console.log(`Results: ${JSON.stringify(response1.data)}`);
     }
 
-    private buildPromises(http: AxiosInstance) {
-        let promises: AxiosPromise<any>[] = [];
-        for(let index of Array.from(Array(10).keys())) {
-            let promise = http.get(`https://jsonplaceholder.typicode.com/posts/${index + 1}`);
-            promises.push(promise);
-        }
-
-        return promises;
-    }
-
-    private buildTask() {
-        return [
-            new Task(500),
-            new Task(300),
-            new Task(200),
-            new Task(100),
-        ]
-    }
 }
 
 const playground = new Playground();
